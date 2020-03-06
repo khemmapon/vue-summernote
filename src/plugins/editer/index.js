3bce29f2abb6615faea3ebc65cc66765f2c17f02
Khemmapon <khemmapon@me.com>
2020-03-06 19:06:38 +0700

    not using bootstrap

--- src/plugins/editer/index.js
+++ src/plugins/editer/index.js
@@ -1,13 +1,15 @@
-var $ = require('jquery')
-require('summernote')
-require('summernote/dist/lang/summernote-zh-CN.min.js')
-exports.install = function (Vue, options) {
+var $ = require('jquery');
+// require('summernote')
+require('summernote/dist/summernote-lite.min.js');
+require('summernote/dist/summernote-lite.min.css');
+require('summernote/dist/lang/summernote-zh-CN.min.js');
+exports.install = function(Vue, options) {
   Vue.component('vue-summernote', {
-    render: function (createElement) {
-      return createElement('div')
+    render: function(createElement) {
+      return createElement('div');
     },
-    mounted: function () {
-      var self = this
+    mounted: function() {
+      var self = this;
       var initOptions = {
         lang: 'zh-CN',
         placeholder: self.placeholder,
@@ -16,40 +18,40 @@ exports.install = function (Vue, options) {
         maxHeight: self.maxHeight,
         focus: self.focus,
         callbacks: {
-          onInit: function () {
-            self.$emit('onInit')
+          onInit: function() {
+            self.$emit('onInit');
           },
-          onEnter: function () {
-            self.$emit('onEnter')
+          onEnter: function() {
+            self.$emit('onEnter');
           },
-          onFocus: function () {
-            self.$emit('onFocus')
+          onFocus: function() {
+            self.$emit('onFocus');
           },
-          onBlur: function () {
-            self.$emit('onBlur')
+          onBlur: function() {
+            self.$emit('onBlur');
           },
-          onKeyup: function (e) {
-            self.$emit('onKeyup', e)
+          onKeyup: function(e) {
+            self.$emit('onKeyup', e);
           },
-          onKeydown: function (e) {
-            self.$emit('onKeydown', e)
+          onKeydown: function(e) {
+            self.$emit('onKeydown', e);
           },
-          onPaste: function (e) {
-            self.$emit('onPaste', e)
+          onPaste: function(e) {
+            self.$emit('onPaste', e);
           },
-          onImageUpload: function (files) {
-            self.$emit('onImageUpload', files)
+          onImageUpload: function(files) {
+            self.$emit('onImageUpload', files);
           },
-          onChange: function (contents) {
-            self.$emit('onChange', contents)
+          onChange: function(contents) {
+            self.$emit('onChange', contents);
           }
         }
-      }
-      var params = Object.assign({}, initOptions, options)
-      $(this.$el).summernote(params)
+      };
+      var params = Object.assign({}, initOptions, options);
+      $(this.$el).summernote(params);
     },
-    beforeDestroy: function () {
-      $(this.$el).summernote('destroy')
+    beforeDestroy: function() {
+      $(this.$el).summernote('destroy');
     },
     props: {
       placeholder: {
@@ -80,13 +82,13 @@ exports.install = function (Vue, options) {
        * @param {String | Number} value
        * @returns {*|jQuery}
        */
-      run: function (code, value) {
+      run: function(code, value) {
         if (typeof value === undefined) {
-          return $(this.$el).summernote(code)
+          return $(this.$el).summernote(code);
         } else {
-          return $(this.$el).summernote(code, value)
+          return $(this.$el).summernote(code, value);
         }
       }
     }
-  })
-}
+  });
+};
